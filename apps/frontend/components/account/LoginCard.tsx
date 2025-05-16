6ba54fdca63c08ecd5ab59d2b911db3768a341e0
codingdestro <codingdestro@gmail.com>
2025-05-16 20:37:42 +0530

    feat: using form for handling user loginCard component

--- apps/frontend/components/account/LoginCard.tsx
+++ apps/frontend/components/account/LoginCard.tsx
@@ -1,5 +1,7 @@
 "use client";
-import LoadingButton from "@/components/LoadingButton";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Card,
   CardContent,
@@ -7,27 +9,80 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
 import React from "react";
+import { Button } from "../ui/button";
+
+const zodSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
 
 const LoginCard = () => {
+  const form = useForm<z.infer<typeof zodSchema>>({
+    resolver: zodResolver(zodSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof zodSchema>) {
+    console.log(data);
+    toast("you have made login request");
+  }
+
   return (
     <>
-      <Card className="w-sm md:w-md">
-        <CardHeader>
-          <CardTitle className="text-center text-2xl">Login</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          <Label>Enter your email</Label>
-          <Input type="email" placeholder="email" />
-          <Label>Enter your password</Label>
-          <Input type="password" placeholder="password" />
-        </CardContent>
-        <CardFooter className="flex items-center justify-end">
-          <LoadingButton status="idle" text="login"></LoadingButton>
-        </CardFooter>
-      </Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Card className="w-sm md:w-md">
+            <CardHeader>
+              <CardTitle className="text-center text-2xl">Login</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="Enter your name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="Enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter className="flex items-center justify-end">
+              <Button type="submit">Login</Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </>
   );
 };
