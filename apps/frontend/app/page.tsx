8b5b6668b79e360534a31691da5b52909df35109
codingdestro <codingdestro@gmail.com>
2025-07-12 15:34:00 +0530

    add: hero section

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,15 +1,9 @@
+import Hero from "@/components/section/Hero";
 
 export default function Home() {
   return (
-    <div className="p-5">
-      <div>
-        <h1 className="text-4xl font-bold text-center">
-          Welcome to PDF-Editor
-        </h1>
-        <p className="text-center font-bold border-dashed border-b border-black">
-          Made by @codingdestro
-        </p>
-      </div>
+    <div className="">
+      <Hero />
     </div>
   );
 }
