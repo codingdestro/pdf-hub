a302fe63088c884068694314633d98072f74c25e
codingdestro <codingdestro@gmail.com>
2025-07-12 16:03:56 +0530

    add: feature section

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedTools from "@/components/section/FeaturedTools";
 import Hero from "@/components/section/Hero";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <FeaturedTools/>
     </div>
   );
 }
