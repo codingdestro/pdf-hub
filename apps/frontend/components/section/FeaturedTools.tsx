01c92631fd6b8557e5cb3120f633d6a07b2dbb19
codingdestro <codingdestro@gmail.com>
2025-07-13 11:56:49 +0530

    add: merge, extract and remove pdf pages

--- apps/frontend/components/section/FeaturedTools.tsx
+++ apps/frontend/components/section/FeaturedTools.tsx
@@ -3,7 +3,7 @@ import FeaturedToolsCard from "./FeaturedToolsCard";
 
 const FeaturedTools = () => {
   return (
-    <section className="p-5 flex flex-col items-center min-h-[60vh] h-full">
+    <section className="p-5 flex flex-col items-center min-h-[60vh] h-full" id="services">
       <div className="text-center py-5">
         <h1 className="text-3xl font-bold">Featured Tools</h1>
       </div>
@@ -11,14 +11,17 @@ const FeaturedTools = () => {
         <FeaturedToolsCard
           url="/icons/merge.png"
           title="Merge two or more PDF into one PDF —"
+          link="/editor/merge"
         />
         <FeaturedToolsCard
           url="/icons/extract.png"
           title="Extract PDF Pages with ease of tools —"
+          link="/editor/extract"
         />
         <FeaturedToolsCard
           url="/icons/remove.png"
           title="Remove PDF Pages from PDF —"
+          link="/editor/extract"
         />
       </div>
     </section>
--- apps/frontend/components/section/FeaturedToolsCard.tsx
+++ apps/frontend/components/section/FeaturedToolsCard.tsx
@@ -1,19 +1,27 @@
+'use client'
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 interface Props {
   url: string;
   title: string;
+  link:string
 }
 
-const FeaturedToolsCard = ({ url, title }: Props) => {
+const FeaturedToolsCard = ({ url, title ,link}: Props) => {
   return (
     <div className="flex flex-col items-center justify-center border rounded-lg shadow-md p-5 space-y-3 w-[340px] h-[232px] box-border ">
       <Image src={url} alt="Merge PDF Icon" width={90} height={90} />
       <p className="text-sm text-center">{title}</p>
-      <Button>Try Now</Button>
+      <Link href={link}>
+        <Button  className="w-full cursor-pointer">
+          Use Tool
+        </Button>
+      </Link>
     </div>
+
   );
 };
 
